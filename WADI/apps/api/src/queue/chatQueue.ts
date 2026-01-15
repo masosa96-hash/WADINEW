a823265990ef733d841d56ae4651de3b4d5a56c2
Matias <masosa96@gmail.com>
2026-01-15 12:00:12 -0300

    chore: clean up deleted files (redis/queue removal)

--- WADI/apps/api/src/queue/chatQueue.ts
+++
@@ -1,7 +0,0 @@
-import { Queue } from "bullmq";
-import { createRedis } from "@wadi/core";
-import type { ChatJobInput } from "@wadi/core";
-
-export const chatQueue = new Queue<ChatJobInput>("chat", {
-  connection: createRedis() as any,
-});
